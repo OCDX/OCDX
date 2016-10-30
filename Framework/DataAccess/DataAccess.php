102971958bac385e6c936bc0a7e670fe746b6681
Brandon Tomblinson <goalieboy17@gmail.com>
2016-10-30 18:47:54 -0500

    Fix manifest

--- Framework/DataAccess/DataAccess.php
+++ Framework/DataAccess/DataAccess.php
@@ -23,10 +23,13 @@ namespace DataAccess {
         }
 
         public function insertManifest($standard_version, $creator, $comment) {
-            $parameters = "$standard_version,$creator,$comment";
+            $this->connection->query("SET @standard_version  = " . "'" . $this->connection->real_escape_string($standard_version) . "'");
+            $this->connection->query("SET @creator  = " . "'" . $this->connection->real_escape_string($creator) . "'");
+            $this->connection->query("SET @comment  = " . "'" . $this->connection->real_escape_string($comment) . "'");
 
-            if (!$this->connection->query("CALL pInsertManifest($parameters)")) {
-                
+            if (!$this->connection->query("CALL pInsertManifest(@standard_version,@creator,@comment)")) {
+                die("CALL failed: (" . $this->connection->errno . ") " . $this->connection->error);
+                return -1;
             }
             return $this->connection->affected_rows;
         }
