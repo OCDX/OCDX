0893dc0548f56827c9ff430bd5db38e4a2f40149
Brandon Tomblinson <goalieboy17@gmail.com>
2016-11-04 13:17:26 -0500

    Fix

--- services/test.php
+++
@@ -1,8 +0,0 @@
-<html>
-
-    <form action="getUser.php" method="GET">
-        <input type="text" name="username" />
-         <input type="text" name="password" />
-        <button type="submit" name="submit">Submit</button>
-    </form>
-</html>
