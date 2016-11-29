217c567f56bd9b81c41ea4208d88a6284389183a
btomblinson <btomblinson@users.noreply.github.com>
2016-11-28 19:40:20 -0600

    Update pSumBytes.sql

--- database/DML/pSumBytes.sql
+++ database/DML/pSumBytes.sql
@@ -1,4 +1,4 @@
 CREATE DEFINER=`daniel`@`%` PROCEDURE `pSumBytes`()
 BEGIN
-    SELECT  SUM(files.size) FROM OCDXGroup1.files WHERE DATE(files.created_on) = CURRENT_DATE;
+    SELECT  SUM(files.size) AS 'Sum' FROM OCDXGroup1.files WHERE DATE(files.created_on) = CURRENT_DATE;
   END
