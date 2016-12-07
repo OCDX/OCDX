2d0f8d149ba3d9e0265f15a107f992b49d969cc6
Brandon Tomblinson <goalieboy17@gmail.com>
2016-12-07 13:07:07 -0600

    Making an actual deployment script

--- deployment/deployment.sh
+++ deployment/deployment.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #create the logging and file upload directories
-sudo yum update && sudo yum install -y MariaDB-server MariaDB-client && sudo /etc/init.d/mysql start && sudo mkdir /publicFiles && sudo mkdir /var/www/logs & sudo chmod 777 /var/www/logs/ & sudo chmod 777 /publicFiles && sudo yum install -y httpd24 php56 php56-mysqlnd && sudo service httpd start;
+sudo yum update && sudo yum install -y MariaDB-server MariaDB-client && sudo /etc/init.d/mysql start && sudo mkdir /publicFiles && sudo mkdir /var/www/logs & sudo chmod 777 /var/www/logs/ & sudo chmod 777 /publicFiles && sudo yum install -y httpd24 php56 php56-mysqlnd && sudo service httpd start && exit;
