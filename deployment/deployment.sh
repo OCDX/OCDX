bb9ae8d05b0465ada44d566b2e5744ee7137eef8
Brandon Tomblinson <goalieboy17@gmail.com>
2016-12-07 13:37:39 -0600

    Making an actual deployment script

--- deployment/deployment.sh
+++ deployment/deployment.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
-#create the logging and file upload directories
-sudo yum update && sudo yum install -y MariaDB-server MariaDB-client && sudo /etc/init.d/mysql start && sudo mkdir /publicFiles && sudo yum install -y httpd24 php56 php56-mysqlnd git && sudo mkdir /var/www/logs && sudo chmod 777 /var/www/logs/ && sudo chmod 777 /publicFiles && sudo service httpd start && sudo chmod 777 /var/www/html/ && cd /var/www/html/ && git clone https://github.com/btomblinson/OCDX.git && mysql -u root < /var/www/html/OCDX/deployment/databaseSetup.sql;
+sudo yum update &&
+sudo yum install -y httpd24 php56 php56-mysqlnd git &&
+sudo chmod 777 /var/www/html/ &&
+cd /var/www/html/  &&
+git clone https://github.com/btomblinson/OCDX.git &&
+cp /var/www/html/OCDX/deployment/maria.rep /etc/yum.repos.d/ &&
+sudo yum install -y MariaDB-server MariaDB-client &&
+sudo /etc/init.d/mysql start &&
+sudo mkdir /publicFiles  &&
+sudo mkdir /var/www/logs &&
+sudo chmod 777 /var/www/logs/ &&
+sudo chmod 777 /publicFiles &&
+sudo service httpd start &&
+mysql -u root < /var/www/html/OCDX/deployment/databaseSetup.sql;
